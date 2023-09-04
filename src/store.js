7aecfba11df592a9526d4efae5e4a5325ca1d3f4
shubhamgoyalll <goyalshubham2521@gmail.com>
2023-09-04 16:38:23 +0530

    Created a Reducer function for redux store

---
+++ src/store.js
@@ -0,0 +1,26 @@
+const initialState = {
+  loan: 0,
+  balance: 0,
+  loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return { ...state };
+      return { ...state, loan: action.payload };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
