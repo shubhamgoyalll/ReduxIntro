f451059b2b2f099e647d37a7e5b67e5b32ca6919
shubhamgoyalll <goyalshubham2521@gmail.com>
2023-09-08 20:30:13 +0530

    Created a Redux store & dispatched some actions

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   loan: 0,
   balance: 0,
@@ -12,7 +14,12 @@ function reducer(state = initialState, action) {
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return { ...state };
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account/payLoan":
       return {
         ...state,
@@ -24,3 +31,21 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+//Creating store by using createStore method from redux
+const store = createStore(reducer);
+
+store.dispatch({ type: "account/deposit", payload: 1000 });
+store.dispatch({ type: "account/withdraw", payload: 300 });
+console.log(store.getState());
+
+//First time passing object in a payload which is fine
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "Buy a Car" },
+});
+
+console.log(store.getState());
+
+store.dispatch({ type: "account/payLoan" });
+console.log(store.getState());
