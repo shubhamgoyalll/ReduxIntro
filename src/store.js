d35983240c4e41a036f24a1b7276df95f04f302b
shubhamgoyalll <goyalshubham2521@gmail.com>
2023-09-08 21:46:57 +0530

    Action Creators Added

--- src/store.js
+++ src/store.js
@@ -35,17 +35,54 @@ function reducer(state = initialState, action) {
 //Creating store by using createStore method from redux
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 1000 });
-store.dispatch({ type: "account/withdraw", payload: 300 });
+// store.dispatch({ type: "account/deposit", payload: 1000 });
+// store.dispatch({ type: "account/withdraw", payload: 300 });
+// console.log(store.getState());
+
+// //First time passing object in a payload which is fine
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a Car" },
+// });
+
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+//We cam use this as a variable in type in dispatch and as a case type in reducer fnc
+// const ACCOUNT_DEPOSIT = "account/deposit";
+
+//action creator -> returns an action
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
+
+store.dispatch(deposit(1000));
 console.log(store.getState());
 
-//First time passing object in a payload which is fine
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a Car" },
-});
-
+store.dispatch(withdraw(300));
 console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(requestLoan(1000, "Buy a Car"));
+console.log(store.getState());
+
+store.dispatch(payLoan());
 console.log(store.getState());
