bb8a15aba7035cbb562405442bd70ca46d8d193c
shubhamgoyalll <goyalshubham2521@gmail.com>
2023-09-09 23:18:48 +0530

    Used State Slices for better folder structure

---
+++ src/features/accounts/accountSlice.js
@@ -0,0 +1,55 @@
+const initialStateAccount = {
+  loan: 0,
+  balance: 0,
+  loanPurpose: "",
+};
+
+//Mostly default export of reducer fnctn
+export default function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan":
+      if (state.loan > 0) return { ...state };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
+}
+
+//action creator -> returns an action
+export function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+//Mostly named export of action creators
+export function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+export function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+export function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
