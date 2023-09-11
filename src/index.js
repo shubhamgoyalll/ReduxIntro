c190bc5046f49fcb8f0cc02f4e0ce177f99e6d2f
shubhamgoyalll <goyalshubham2521@gmail.com>
2023-09-12 00:20:57 +0530

    Connected Redux with React app and used Store in compon

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { Provider } from "react-redux";
 import store from "./store";
 
 store.dispatch({ type: "account/deposit", payload: 1000 });
@@ -11,6 +12,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
