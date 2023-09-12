c25afefeb744377ccaa969d67bd5f276c44684dc
shubhamgoyalll <goyalshubham2521@gmail.com>
2023-09-13 00:16:04 +0530

    accountSlice by using RTK modern way using createSlice

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,12 +1,61 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   loan: 0,
   balance: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
+const accountSlice = createSlice({
+  name: "account", // Slice name
+  initialState, //initial state that we have give
+  reducers: {
+    // Object full of reducer functions
+    deposit(state, action) {
+      //Now we can write mutating logic
+      state.balance = state.balance + action.payload;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+
+    requestLoan: {
+      //prepare method used to take two argument as in RTK action creator can only have one argument by default
+      prepare(amount, purpose) {
+        return {
+          payload: {
+            amount,
+            purpose,
+          },
+        };
+      },
+
+      reducer(state, action) {
+        if (state.loan > 0) return;
+
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
+    },
+    payLoan(state, action) {
+      state.balance = state.balance - state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+  },
+});
+
+// console.log(accountSlice);
+
+export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export default accountSlice.reducer;
+
+/*
 //Mostly default export of reducer fnctn
-export default function accountReducer(state = initialStateAccount, action) {
+export default function accountReducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
       return {
@@ -76,3 +125,4 @@ export function payLoan() {
     type: "account/payLoan",
   };
 }
+*/
